104809acc22a6c66c4561a49751f5f610a5124ba
Curious Demon <118127510+CuriousDemon@users.noreply.github.com>
2023-08-25 18:18:47 +0530

    Delete Password Manager Directory

--- Password Manager/signupform/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SignupformConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'signupform'
--- Password Manager/signupform/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from signupform import views
-urlpatterns = [
-    path('', views.signup_page),
-    path('signin/', views.signin_page),
-    path('signin/dashboard/', views.dashboard),
-    path('signin/dashboard/note/', views.manager),
-]
